33241090c1d83e6045fd6a5abe1ac8ff077c694e
Gowthama-V <gowthama@venzotechnologies.com>
2022-08-08 14:17:40 +0530

    Dm Page

--- src/component/DmPage/DM_Page.js
+++ src/component/DmPage/DM_Page.js
@@ -1,12 +1,18 @@
-import React from 'react'
-import Header from '../header/header'
+import React, { useState } from 'react';
+import Fade from 'react-bootstrap/Fade'; import Header from '../header/header'
+import Collapse from 'react-bootstrap/Collapse'
+import { Accordion, Card, Button } from "react-bootstrap";
+
 import block from '../../images/Blockchain.png'
 import './DM_page.css'
 import colorArrow from '../../images/Read more arrow-gradient.svg'
 import line from '../../images/Vector 76.svg'
 import Service from './service'
 import Dmcarousels from './DmCarousel'
+import FrequntQus from '../../content/ferquntQus.json'
 function DM_Page() {
+    const [open, setOpen] = useState(false);
+
     return (
         <>
             <Header flag='header1' />
@@ -49,22 +55,68 @@ function DM_Page() {
                 </div>
                 <div className='section5'>
                     <p className='sect5_title'> What you get</p> </div>
+                <div className='cardList'>
                     <ul className='DM_cardTitle'>
-                        <li>Brand Promotion</li>
-                        <li>Increase Sales</li>
-                        <li>Lead Generation</li>
-                        <li>SE ranking</li>
-                        <li>Online Reputation</li>
-                        <li>Social media engagement</li>
+                        <li className='cardtitle1' onClick={() => dated('brand')}>Brand Promotion</li>
+                        <li className='cardtitle2' onClick={() => dated('sales')}>Increase Sales</li>
+                        <li className='cardtitle3' onClick={() => dated('lead')}>Lead Generation</li>
+                        <li className='cardtitle4' onClick={() => dated('se')}>SE ranking</li>
+                        <li className='cardtitle5' onClick={() => dated('online')}>Online Reputation</li>
+                        <li className='cardtitle6' onClick={() => dated('social')}>Social media engagement</li>
                     </ul>
+                    <div className='card1'>
+
+                    </div>
                 </div>
-                <div >
-                    <Dmcarousels />
-                </div>
-                
+            </div>
+            <Dmcarousels />
+            <div className='section6'>
+                <p className='sect6_title'>Frequently asked questions?</p>
+                {FrequntQus.map(item => {
+                    return <div key={item.id} defaultActiveKey="0">
+                    {console.log(item.id,item.titile,item.data)}
+                        <p>{item.titile}</p>
+                        <Button variant="link" Key={item.id} onClick={() => setOpen(!open)} aria-expanded={open} aria-controls="collapseID"> +  </Button>
+                        <Collapse in={open} Key={item.id}>
+                            <div 
+                                style={{
+                                    width: 300,
+                                    textAlign: 'justify'
+                                }}
+                            >
+                                {item.data}
+                            </div>
+                        </Collapse>
+                    </div>
+                })}
+
+            </div>
+
 
         </>
     )
+}
+function dated(va) {
+    { console.log(va) }
+    let container = document.querySelector('.card1')
+    if (va == 'brand') {
+        container.innerHTML = 'brand1111'
+    }
+    else if (va === 'sales') {
+        container.innerHTML = 'brand2222'
+    }
+    else if (va === 'lead') {
+        container.innerHTML = 'brand3333'
+    }
+    else if (va === 'se') {
+        container.innerHTML = 'brand4444'
+    }
+    else if (va === 'online') {
+        container.innerHTML = 'brand5555'
+    }
+    else if (va === 'social') {
+        container.innerHTML = 'brand6666'
+    }
 }
 
 export default DM_Page
--- src/component/DmPage/DmCarousel.js
+++ src/component/DmPage/DmCarousel.js
@@ -2,37 +2,77 @@ import React from 'react'
 import Carousel from 'react-bootstrap/Carousel';
 import Dm_Carousel from "../../content/DmCarousel.json"
 import surcamp from "../../DMpage/image 7.png"
-import sirius from "../../DMpage/image 8.png"  
-import agsHealth from "../../DMpage/image 10.png"  
-import panPure from "../../DMpage/image 11.png"  
-import  btsscrop from "../../DMpage/image 13.png"  
-import mangopoint from "../../DMpage/image 14.png"  
+import sirius from "../../DMpage/image 8.png"
+import agsHealth from "../../DMpage/image 10.png"
+import panPure from "../../DMpage/image 11.png"
+import btsscrop from "../../DMpage/image 13.png"
+import mangopoint from "../../DMpage/image 14.png"
 function DmCarousel() {
     return (
         <div>
-    <Carousel fade className='DmCarosel' interval={3000}>
+            <Carousel fade className='DmCarosel' interval={3000}>
                 <Carousel.Item >
-                <div >
-                <img className='line1' src={surcamp}/>
-                <img className='line1' src={sirius}/>
-                <img className='line1' src={agsHealth}/>
-                <img className='line1' src={panPure}/>
-                <img className='line1' src={btsscrop}/>
-                <img className='line1' src={mangopoint}/>
-                </div>
+                    <div >
+                        <img className='line1' src={surcamp} />
+                        <img className='line1' src={sirius} />
+                        <img className='line1' src={agsHealth} />
+                        <img className='line1' src={panPure} />
+                        <img className='line1' src={btsscrop} />
+                        <img className='line1' src={mangopoint} />
+                    </div>
                 </Carousel.Item>
                 <Carousel.Item >
-                <div >
-                <img className='line1' src={panPure}/>
-                <img className='line1' src={btsscrop}/>
-                <img className='line1' src={mangopoint}/>
-                <img className='line1' src={surcamp}/>
-                <img className='line1' src={sirius}/>
-                <img className='line1' src={agsHealth}/>
-                
-                </div>
-                                </Carousel.Item>
-                
+                    <div >
+                        <img className='line1' src={sirius} />
+                        <img className='line1' src={agsHealth} />
+                        <img className='line1' src={panPure} />
+                        <img className='line1' src={btsscrop} />
+                        <img className='line1' src={mangopoint} />
+                        <img className='line1' src={surcamp} />
+
+                    </div>
+                </Carousel.Item>
+                <Carousel.Item >
+                    <div >
+                        <img className='line1' src={agsHealth} />
+                        <img className='line1' src={panPure} />
+                        <img className='line1' src={btsscrop} />
+                        <img className='line1' src={mangopoint} />
+                        <img className='line1' src={surcamp} />
+                        <img className='line1' src={sirius} />
+                    </div>
+                </Carousel.Item>                
+                <Carousel.Item >
+                    <div >
+                        <img className='line1' src={panPure} />
+                        <img className='line1' src={btsscrop} />
+                        <img className='line1' src={mangopoint} />
+                        <img className='line1' src={surcamp} />
+                        <img className='line1' src={sirius} />
+                        <img className='line1' src={agsHealth} />
+                    </div>
+                </Carousel.Item>                
+                <Carousel.Item >
+                    <div >
+                        <img className='line1' src={btsscrop} />
+                        <img className='line1' src={mangopoint} />
+                        <img className='line1' src={surcamp} />
+                        <img className='line1' src={sirius} />
+                        <img className='line1' src={agsHealth} />
+                        <img className='line1' src={panPure} />
+                    </div>
+                </Carousel.Item>                
+                <Carousel.Item >
+                    <div >
+                        <img className='line1' src={mangopoint} />
+                        <img className='line1' src={surcamp} />
+                        <img className='line1' src={sirius} />
+                        <img className='line1' src={agsHealth} />
+                        <img className='line1' src={panPure} />
+                        <img className='line1' src={btsscrop} />
+                    </div>
+                </Carousel.Item>
+
             </Carousel>
         </div>
 
