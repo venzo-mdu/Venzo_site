cb4e1977fcf92ca8529a1909bc60c812f46ce512
priyachandrasekar <priyankac@venzotechnologies.com>
2022-12-19 18:57:02 +0530

    blogpage

---
+++ src/component/Blogs/angularvsreact.js
@@ -0,0 +1,90 @@
+import React from 'react'
+import tweet1 from '../../images/blogsPic/tweet1.png'
+import tweet2 from '../../images/blogsPic/tweet2.png'
+import angularreact from '../../images/blogsPic/AngularVSReact.webp'
+
+import '../Blogs/angularvsreact.css'
+import '../Blogs/angularvsreactRes.css'
+
+function angularvsreact() {
+  return (
+    <div>
+        <div className='angularBlog'>
+            <div className='angularLef'>
+                    <p className='angulatTitle1'>Angular JS vs React Js: Which is Better for You?</p>
+                    <div className='angulatTitle1flex'>
+                        <p className='AngularDate'>May 26th, 2022    |</p>
+                        <p className='AnguarPrd'>Product Development</p>
+                    </div>
+                    <img src={angularreact} className='angularvsreact'/>
+                    <p className='angularCont'>The ability to develop robust and reliable web applications in a shorter time using a high functionality framework is the common requirement among modern business enterprises, regardless of their size and type. The two major frameworks that are being used extensively in the contemporary tech world are Angular JS and React JS.</p>
+                    <p className='Significance '>The Significance of Choosing Between Angular JS and React JS</p>
+                    <p className='angularCont'>Both Angular JS and React JS contribute to the tech-driven business ecosystem. These two are the most popular app development frameworks that facilitate businesses to achieve their unique objectives. To be able to identify which one suits you better, it is essential to understand the advantages and unique characteristics of each. Each business has its own unique development requirements. There is no ‘one bill fits all’ solution for powerful web development solutions. These necessitate a detailed analysis of Angular JS web development and React JS web development. A comparative study enables selecting the framework that works best for your business.</p>
+                    <p className='Significance'>Understanding the Nuances of Web Development Using Angular JS and React JS for Choosing the Right One</p>
+                    <p className='Significance'>Angular JS</p>
+                    <p className='angularCont'> Angular JS, the JavaScript web application framework, has been originally created by Google and released in 2010. With support from Google and an extensive community of developers, Angular JS aims at pooling familiar and popular patterns, designs, development practices, and components into a single framework. This enormously helps in addressing challenges associated with single-page application development.
+
+Angular JS supports quite a few consistent mechanisms such as views, models, services, and controllers. This has rendered it to use the crucial software development patterns of model view controller (MVC) and dependency injection, thus enabling faster web applications development.</p>
+                    <p className='Significance'>React JS</p>
+                    <p className='angularCont'>React JS is more of an open-source JavaScript library than a framework. Developed by Facebook in 2013, React JS has been launched with the primary purpose of resolving issues related to rendering large datasets while ensuring commendable app performance. React JS web development depends more on the ‘view’ aspect in the MVC architecture. It is mainly used for operating the dynamic user interface of web pages that have high incoming traffic.
+
+Although it is a new framework, React JS is being used by several huge enterprises such as Facebook, Instagram, Discovery, and Netflix for app development.</p>
+                    <p className='Significance'>Exclusive Features of Angular JS</p>
+                    <p className='angularCont'>
+                        <ul>
+                            <li className='angularCont'>Model View Controller (MVC) Framework</li>
+                            <li className='angularCont'>POJO Model</li>
+                            <li className='angularCont'>Data-binding</li>
+                            <li className='angularCont'>User interface with HTML</li>
+                            <li className='angularCont'>Active Google community</li>
+                            <li className='angularCont'>Dependency Injection</li>
+                            <li className='angularCont'>Routing</li>
+                        </ul>
+                    </p>
+                    <p className='Significance'>Exclusive Features of React JS</p>
+                    <p className='angularCont'>
+                    <ul>
+                            <li className='angularCont'>One-way Data Binding</li>
+                            <li className='angularCont'>Superior Performance</li>
+                            <li className='angularCont'>Multiple Components</li>
+                            <li className='angularCont'>JSX Syntax</li>
+                            <li className='angularCont'>Virtual DOM</li>
+                            <li className='angularCont'>Simplicity</li>
+                        </ul>
+                    </p>
+                    <p className='Significance'>A Comparative Analysis of Angular JS and React JS</p>
+                    <p className='angularCont'>
+                        <ul >
+                            <li className='angularCont angularCont1'><strong>Components & Size: </strong>Angular allows MVC pattern; it works on Real DOM. It follows bidirectional data flow and is large in size compared with React. On the other hand, React uses virtual DOM. It follows unidirectional data flow. The size of React is smaller than that of Angular.</li>
+                            <li className='angularCont angularCont1'><strong>Learning Curve: </strong>Angular being a full-blow framework, it takes time and effort for anyone to learn its nuances. There are several concepts and patterns that necessitate in-depth learning. React, being a library, and having fewer concepts, offers itself to be learned with ease and in a short time.</li>
+                            <li className='angularCont angularCont1'><strong>Community Support and Migration:</strong> Every six months, Angular releases a new version. The two updates or major releases in a year ensure to make essential changes. Moreover, an active and huge community supports Angular. In the case of React JS—with stability being the major concern with Facebook—upgrades are easier than those of Angular. React has good community support as well.</li>
+                            <li className='angularCont angularCont1'><strong>Scalability: </strong>The design and powerful command-line interface of Angular make it is easy to scale. React, with its virtual DOM and library, accommodates scalable development too. It is one of the favored view frameworks for building reusable and scalable components.</li>
+                            <li className='angularCont angularCont1'><strong>Development Lifecycle: </strong>Angular extends the application of HTML, reducing the learning dependency. It offers seamless development possibilities without the need for much effort. In React JS, as every entity acts as an independent unit with unique logic, changes made to a part will not impact other parts. Time to market can be accelerated, as updates are easy, and developers can reuse the elements.</li>
+                        </ul>
+                    </p>
+                    <p className='Significance'>Conclusion</p>
+                    <p className='angularCont'>Both Angular JS and React JS have their benefits and unique characteristics. Developers who look for simplicity and flexibility may go in for React JS, while those who need the best way of organizing and boosting their applications with a comprehensive tool might want to choose Angular JS. Venzo is a leading web application development service provider having skilled and experienced Angular JS and React JS professionals.</p>
+            </div>
+            <div className='blogsRight1'>
+                    <div className='slide1'>
+                        <p className='angularCont'>All Categories</p> <hr />
+                        <p className='angularCont'>Automated Testing</p><hr />
+                        <p className='angularCont'>Mobile App Development</p><hr />
+                        <p className='angularCont'>Mobile App Development</p><hr />
+                        <p className='angularCont'>Mobile App Development</p><hr />
+                        <p className='angularCont'>Staff Augmentation</p><hr />
+                        <p className='angularCont'>Technology</p><hr />
+                        <p className='angularCont'>Web App Development</p>
+                    </div>
+                    <div className='slide2'>
+                        <p>Tweets</p>
+                        <img src={tweet1} />
+                        <img src={tweet2} />
+                    </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default angularvsreact
