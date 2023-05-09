527aa8cfb5c2c81642b76c0f1ccd3edd91bef9b1
Harisht2811 <harisht@venzotechnologies.com>
2023-05-09 10:30:48 +0530

    May 9 10.30

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,13 +10,13 @@ module.exports = {
         id: "GTM-K4XC96N",
         includeInDevelopment: false,
         defaultDataLayer: { platform: "gatsby" },
-        gtmAuth: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_AUTH_STRING",
-        gtmPreview: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_PREVIEW_NAME",
-        dataLayerName: "YOUR_DATA_LAYER_NAME",
-        routeChangeEventName: "YOUR_ROUTE_CHANGE_EVENT_NAME",
-        enableWebVitalsTracking: true,
-        selfHostedOrigin: "YOUR_SELF_HOSTED_ORIGIN",
+
+        defaultDataLayer: function () {
+          return {
+            pageType: window.pageType,
+          }
+        },
       },
     },
-  ]
+  ],
 };
